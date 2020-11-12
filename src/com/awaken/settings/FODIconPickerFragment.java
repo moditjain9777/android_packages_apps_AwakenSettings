AOSPA-custom/AOSPA-custom-android_packages_apps_CustomSettings-

CustomSettings: FOD icon picker [2/2]
Signed-off-by: brjshshrma <brajesh.sharma87@gmail.com>

src/com/custom/settings/fragments/FODIconPickerFragment.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2020 The Evolution X Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.custom.settings.fragments;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class FODIconPickerFragment extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.settings_fod_picker);
+
+        getActivity().getActionBar().setTitle(R.string.fod_icon_picker_title);
+
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.fod_icon_picker_footer);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM_SETTINGS;
+    }
+}

src/com/custom/settings/util/CustomContextConstants.java
@@ -1,0 +1,47 @@
+/**
+ * Copyright (c) 2015, The CyanogenMod Project
+ * Copyright (c) 2020, RR
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.custom.settings.util;
+
+import android.annotation.SdkConstant;
+
+/**
+ * @hide
+ * TODO: We need to somehow make these managers accessible via getSystemService
+ */
+public final class CustomContextConstants {
+
+    /**
+     * @hide
+     */
+    private CustomContextConstants() {
+        // Empty constructor
+    }
+
+    /**
+     * Features supported by RR
+     */
+    public static class Features {
+        /**
+         * Feature for {@link PackageManager#getSystemAvailableFeatures} and
+         * {@link PackageManager#hasSystemFeature}: The device includes the pa
+         * in-screen fingerprint.
+         */
+        @SdkConstant(SdkConstant.SdkConstantType.FEATURE)
+        public static final String FOD = "vendor.pa.biometrics.fingerprint.inscreen";
+    }
+}

moditjain9777/android_packages_apps_AwakenSettings

src/com/awaken/settings/FODIconPickerFragment.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (C) 2020 The Evolution X Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.awaken.settings;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class FODIconPickerFragment extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.settings_fod_picker);
+
+        getActivity().getActionBar().setTitle(R.string.fod_icon_picker_title);
+
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.DIRTYTWEAKS;
+    }
+}

src/com/awaken/settings/util/CustomContextConstants.java
@@ -1,0 +1,47 @@
+/**
+ * Copyright (c) 2015, The CyanogenMod Project
+ * Copyright (c) 2020, RR
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.awaken.settings.util;
+
+import android.annotation.SdkConstant;
+
+/**
+ * @hide
+ * TODO: We need to somehow make these managers accessible via getSystemService
+ */
+public final class CustomContextConstants {
+
+    /**
+     * @hide
+     */
+    private CustomContextConstants() {
+        // Empty constructor
+    }
+
+    /**
+     * Features supported by RR
+     */
+    public static class Features {
+        /**
+         * Feature for {@link PackageManager#getSystemAvailableFeatures} and
+         * {@link PackageManager#hasSystemFeature}: The device includes the pa
+         * in-screen fingerprint.
+         */
+        @SdkConstant(SdkConstant.SdkConstantType.FEATURE)
+        public static final String FOD = "vendor.pa.biometrics.fingerprint.inscreen";
+    }
+}